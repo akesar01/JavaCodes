1becdb4699eb48ab83072d500417b77a9185119d
Ankit Kesar <56121226+mrawk@users.noreply.github.com>
2020-09-22 18:16:47 +0530

    Create FastScanner.java

---
+++ FastScanner.java
@@ -0,0 +1,62 @@
+class FastScanner {
+    private BufferedReader reader = null;
+    private StringTokenizer tokenizer = null;
+    public FastScanner(InputStream in) {
+        reader = new BufferedReader(new InputStreamReader(in));
+        tokenizer = null;
+    }
+ 
+    public String next() {
+        if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                tokenizer = new StringTokenizer(reader.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return tokenizer.nextToken();
+    }
+ 
+    public String nextLine() {
+        if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                return reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return tokenizer.nextToken("\n");
+    }
+ 
+    public long nextLong() {
+        return Long.parseLong(next());
+    }
+ 
+    public int nextInt() {
+        return Integer.parseInt(next());
+    }
+ 
+    public double nextDouble() {
+         return Double.parseDouble(next());
+    }
+    
+    public String[] nextArray(int n) {
+        String[] a = new String[n];
+        for (int i = 0; i < n; i++)
+            a[i] = next();
+        return a;
+    }
+ 
+    public int[] nextIntArray(int n) {
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = nextInt();
+        return a;
+    }
+ 
+    public long[] nextLongArray(int n) {
+        long[] a = new long[n];
+        for (int i = 0; i < n; i++)
+            a[i] = nextLong();
+        return a;
+    } 
